52b36d1fe4a250bcd9a9663265de0619479c2986
Savanah Steigen <savanahsteigen@gmail.com>
2020-04-20 15:47:45 -0400

    Updates

---
+++ precache-manifest.690ba9cf4f0eee18b27b284cd19aa8e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7e414a05c8d56f1d13ddb357458a0a",
+    "url": "/propgress/index.html"
+  },
+  {
+    "revision": "49b56e963f4c4c9a94f0",
+    "url": "/propgress/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f540c4d2ef8d64f5d5e2",
+    "url": "/propgress/static/js/2.d811afa8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/propgress/static/js/2.d811afa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b56e963f4c4c9a94f0",
+    "url": "/propgress/static/js/main.ee8c9835.chunk.js"
+  },
+  {
+    "revision": "fcf093ab8aa2c48791e2",
+    "url": "/propgress/static/js/runtime-main.b6b9aee7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/propgress/static/media/logo.5d5d9eef.svg"
+  }
+]);
